247a0966d7e12ea703c64e7a728c3c42f02da08d
Norman <2862129949@qq.com>
2018-08-02 10:11:11 +0800

    local first commit

---
+++ src/bean/Car.java
@@ -0,0 +1,29 @@
+package bean;
+
+public class Car {
+
+	private String color;
+	
+	private String price;
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Car [color=" + color + ", price=" + price + "]";
+	}
+}
